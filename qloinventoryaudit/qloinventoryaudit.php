852a7d6b4efbcd0c1cda9639b18a4808b65546fb
Manuel Ferraz <manuelferraz@google.com>
2026-09-09 15:30:54 +0000

    feat(inventory-audit): setup module definition and back-office tab registration

--- qloinventoryaudit/qloinventoryaudit.php
+++ qloinventoryaudit/qloinventoryaudit.php
@@ -1,5 +1,12 @@
 <?php
-// modules/qloinventoryaudit/qloinventoryaudit.php
+/**
+ * QloInventoryAudit Module
+ *
+ * Módulo para auditoria de sobreposição de inventário e integridade física de quartos.
+ *
+ * @author QloApps Engineering
+ * @license AFL-3.0
+ */
 
 if (!defined('_PS_VERSION_')) {
     exit;
@@ -20,38 +27,70 @@ class QloInventoryAudit extends Module
 
         $this->displayName = $this->l('Auditor de Sobreposição de Inventário');
         $this->description = $this->l('Detecção de conflitos e duplas alocações de quartos físicos.');
+        $this->ps_versions_compliancy = ['min' => '1.6', 'max' => _PS_VERSION_];
     }
 
+    /**
+     * Instala o módulo e registra a aba no menu administrativo.
+     *
+     * @return bool
+     */
     public function install()
     {
         return parent::install() && $this->installTab();
     }
 
+    /**
+     * Desinstala o módulo e remove a aba do menu administrativo.
+     *
+     * @return bool
+     */
     public function uninstall()
     {
         return $this->uninstallTab() && parent::uninstall();
     }
 
+    /**
+     * Cria e registra a aba administrativa 'AdminInventoryAudit' no menu de pedidos/reservas.
+     *
+     * @return bool
+     */
     private function installTab()
     {
+        $tabId = (int) Tab::getIdFromClassName('AdminInventoryAudit');
+        if ($tabId) {
+            return true;
+        }
+
         $tab = new Tab();
         $tab->active = 1;
         $tab->class_name = 'AdminInventoryAudit';
-        $tab->name = array();
+        $tab->name = [];
         foreach (Language::getLanguages(true) as $lang) {
-            $tab->name[$lang['id_lang']] = 'Auditor de Conflitos';
+            $tab->name[$lang['id_lang']] = $this->l('Auditor de Conflitos');
         }
-        $tab->id_parent = (int) Tab::getIdFromClassName('AdminParentOrders');
+
+        $idParent = (int) Tab::getIdFromClassName('AdminParentOrders');
+        if (!$idParent) {
+            $idParent = (int) Tab::getIdFromClassName('AdminHotelReservation');
+        }
+        $tab->id_parent = $idParent;
         $tab->module = $this->name;
-        return $tab->add();
+
+        return (bool) $tab->add();
     }
 
+    /**
+     * Remove a aba administrativa ao desinstalar o módulo.
+     *
+     * @return bool
+     */
     private function uninstallTab()
     {
         $idTab = (int) Tab::getIdFromClassName('AdminInventoryAudit');
         if ($idTab) {
             $tab = new Tab($idTab);
-            return $tab->delete();
+            return (bool) $tab->delete();
         }
         return true;
     }
